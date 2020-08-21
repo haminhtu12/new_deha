3b1dea60edc1c8f87b25e67471444da1702d8ece
Ha Minh Tu <haminhtu1221@gmail.com>
2020-08-21 08:53:56 +0700

    fix layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,13 +7,11 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
     <title>BookShop</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <!-- GOOGLE FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500" rel="stylesheet"/>
     <link href="https://cdn.materialdesignicons.com/3.0.39/css/materialdesignicons.min.css" rel="stylesheet" />
-
     <!-- PLUGINS CSS STYLE -->
     <link href="{{asset('assets/plugins/toaster/toastr.min.css')}}" rel="stylesheet" />
     <link href="{{asset('assets/plugins/nprogress/nprogress.css')}}" rel="stylesheet" />
@@ -22,15 +20,10 @@
     <link href="{{asset('assets/plugins/ladda/ladda.min.css')}}" rel="stylesheet" />
     <link href="{{asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet" />
     <link href="{{asset('assets/plugins/daterangepicker/daterangepicker.css')}}" rel="stylesheet" />
-
     <!-- SLEEK CSS -->
     <link id="sleek-css" rel="stylesheet" href="{{asset('assets/css/sleek.css')}}" />
-
-
-
     <!-- FAVICON -->
     <link href="{{asset('assets/img/favicon.png')}}" rel="shortcut icon" />
-
     <!--
       HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
     -->
@@ -41,13 +34,9 @@
     <![endif]-->
     <script src="assets/plugins/nprogress/nprogress.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-
     <script src = "{{asset('admin/js/my_js.js')}}"></script>
     <script src="{{asset('ckeditor/ckeditor.js')}}"></script>
-
 </head>
-
-
 <body class="sidebar-fixed sidebar-dark header-fixed header-light" id="body">
 {{--<script>--}}
 {{--    NProgress.configure({showSpinner: false});--}}
@@ -55,9 +44,7 @@
 {{--</script>--}}
 
 <div class="mobile-sticky-body-overlay"></div>
-
 <div class="wrapper">
-
     @include('layouts.slide_bar')
     <div class="page-wrapper">
         @include('layouts.header')
@@ -65,8 +52,6 @@
         @include('layouts.footer')
     </div>
 </div>
-
-
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCn8TFXGg17HAUcNpkwtxxyT9Io9B_NcM" defer></script>
 <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
 <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
@@ -87,8 +72,6 @@
 <script src="{{asset('assets/js/date-range.js')}}"></script>
 <script src="{{asset('assets/js/map.js')}}"></script>
 <script src="{{asset('assets/js/custom.js')}}"></script>
-
-
 </body>
 </html>
 
